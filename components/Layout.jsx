272f20e58c0ec274ff8259e1baeef4f4fb0e723a
Lalo0724 <87767054+Lalo0724@users.noreply.github.com>
2021-09-02 09:24:43 -0500

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import utilStyles from '../styles/utils.module.css';
-import styles from '../styles/layout.module.css';
+import styles from '../styles/Layout.module.css';
 
 const name = "Bluweb";
 
